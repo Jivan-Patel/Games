c6c57e79885dc25a4fb653c372ff6b32728fddc8
Patel-Jivan <jivangj0@gmail.com>
2025-11-13 14:59:04 +0530

    Update the Game

--- 01CounterClick/script.js
+++ 01CounterClick/script.js
@@ -20,10 +20,17 @@ var track = false;
 var cps = 0;
 
 var idTracker = null;  // time control
+var player = localStorage.getItem('userName');
+if(player == null){
+    player = prompt("Enter Your Name");
+    localStorage.setItem('userName',player);
+}
 
-// var player = prompt("Enter Your Name");
-
-
+const styleObj1 = {
+    color: "#ff3b3b",
+    textShadow: "2px 2px 6px rgba(0, 0, 0, 0.45)",
+    scale: "1.25"
+}
 
 function loadContent() {         // Load content when game open
     dataLoad();
@@ -48,49 +55,61 @@ function displayMessage() {      // To display / change content
     highScore.textContent = high;
     timer.textContent = time1;
     cpsScore.textContent = cps;
+
+
+    if (current > 20) {
+        Object.assign(currentScore.style, styleObj1)
+    }
 };
 
 function statusMsg(msg) {
     statusmessage.textContent = msg;
 }
 
-function endGame(){
+function endGame() {
     clearInterval(id);
     track = false;
     clickButton.disabled = true;
     startButton.disabled = false;
     pauseButton.disabled = true;
+    resetButton.disabled = false;
+    clickButton.style.transform = `scale(1)`
+    statusMsg(`You clicked ${cps} times per second!`);
+    startButton.innerHTML = "Play Again"
 
-    if(current > high){
-        localStorage.setItem('highScore',current);
+
+    if (current > high) {
+        localStorage.setItem('highScore', current);
         high = current;
         displayMessage();
-        alert("You Won the Game!");
-        // statusMsg(`Congratulations ${player} you Won The game`);
-        vdocontainer.style.opacity = 1
-        
+        statusMsg(`Congratulations ${player} you Won The game`);
+        // vdocontainer.style.opacity = 1;
+        document.body.style.background = 'gold'
+        setTimeout(()=>{
+            document.body.style.background = 'var(--primary-color)'
+        },2000)
+
     }
-    else if(current == high){
-        // statusMsg(` ${player} Your current is equal to previous one`);
-        alert("You score is Tie!");
+    else if (current == high) {
+        statusMsg(` ${player} Your current is equal to previous one`);
     }
-    else{
-        // statusMsg(`${player} Your current score is less compare to previous high`); 
-        alert("Better Luck Next time");
+    else {
+        statusMsg(`${player} Your current score is less compare to previous high`); 
     }
 
 }
 
-function gameResume(){
+function gameResume() {
     track = true;
     clickButton.disabled = false;
     startButton.disabled = true;
     pauseButton.disabled = false;
-    statusMsg("Game is Started...");
+    statusMsg("Click on Click Me button!...");
 
     id = setInterval(function () {
         time1--;
-        cps = Math.round(current/(10-time1));
+        
+        time1!= 10 ? cps = (current / (10 - time1)).toFixed(1) :cps = current
         if (time1 <= 0) {
             endGame();
         }
@@ -103,6 +122,11 @@ function startGame() {
     time1 = 10;
     current = 0;
     cps = 0;
+    resetButton.disabled = true;
+    clickButton.innerHTML = "Click Me!";
+    setTimeout(() => {
+        clickButton.innerHTML = ""
+    }, 1000)
     displayMessage();
     gameResume();
 }
@@ -110,31 +134,35 @@ function startGame() {
 function clickMe() {
     if (track) {
         current++;
+        if (current <= 10) {
+            const scale = 1 + current / 10;
+            clickButton.style.transform = `scale(${scale})`
+        }
         displayMessage();
     }
 }
 
-function resetHighScore(){
-    localStorage.clear();                   // Delete High score from local storage 
-    high = 0;                               // reset high variable 
-    vdocontainer.style.opacity = 0;           
+function resetHighScore() {
+    localStorage.removeItem('highScore');       // Delete High score from local storage 
+    high = 0;                                   // reset high variable 
+    vdocontainer.style.opacity = 0;
     statusMsg("Your high score is now reset");
-    if(time1<=0){
-        current = 0;  
-        cps = 0;                        // reset current variable
+    if (time1 <= 0) {
+        current = 0;
+        cps = 0;                                // reset current variable
     }
     displayMessage();
 
 }
 
-function pauseGame(){
+function pauseGame() {
     clickButton.disabled = true;
     pauseButton.disabled = true;
     resumeButton.disabled = false;
     clearInterval(id);
 }
 
-function resumeGame(){
+function resumeGame() {
     clickButton.disabled = false;
     pauseButton.disabled = false;
     resumeButton.disabled = true;
@@ -146,6 +174,6 @@ function resumeGame(){
 loadContent();
 startButton.addEventListener("click", startGame);
 clickButton.addEventListener("click", clickMe);
-resetButton.addEventListener("click",resetHighScore);
+resetButton.addEventListener("click", resetHighScore);
 pauseButton.addEventListener("click", pauseGame);
-resumeButton.addEventListener("click",resumeGame);
+resumeButton.addEventListener("click", resumeGame);
